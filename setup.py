db0c10bcd0594288c47a41405241eea1c564c2a0
Simon Käser <skaeser@raptus.com>
2013-06-25 10:39:24 +0200

    initial package structure

---
+++ setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0a1.dev0'
+
+long_description = (
+    open("README.rst").read() + "\n" +
+    open(os.path.join("docs", "MANUAL.txt")).read() + "\n" +
+    'Contributors\n'
+    '============\n'
+    + '\n' +
+    open('CONTRIBUTORS.txt').read()
+    open("CHANGES.txt").read())
+
+setup(name='raptus.article.mediagallery',
+      version=version,
+      description="Provides a horizontal and optionally grouped media gallery able to render images and movies",
+      long_description=long_description,
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Raptus AG',
+      author_email='dev@raptus.com',
+      url='http://github.com/Raptus/raptus.article.mediagallery',
+      license='gpl',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['raptus', 'raptus.article'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'raptus.article.core',
+          'raptus.article.images',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
